6de44915d3fad859505e0efdb36159a21110c898
Makoto Tanabe <xekid78@outlook.com>
2018-04-06 23:31:28 +0900

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mako1.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("Hello World")
+    }
+}
